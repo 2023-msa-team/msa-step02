1e888d8693776baac953877a5d497763db101e5a
metacoding <getinthere@naver.com>
2023-08-30 14:50:27 +0900

    msa 유레카 서버, 유레카 클라이언트, API Gateway

---
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -0,0 +1,34 @@
+package com.example.firstservice;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/first-service")
+@RestController
+public class FirstServiceController {
+
+    private final Environment env;
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to the First service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "Message to the First service";
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return String.format("Check First Service on PORT %s", env.getProperty("local.server.port"));
+    }
+}
